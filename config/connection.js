539b38cff6864acf838f72dde2292659ec392be6
Michael <michael.zaccaria1@gmail.com>
2019-12-02 10:16:43 -0800

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "hcm4e9frmbwfez47.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
   port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
+  user: "zbrfwbj9lxgtfrij",
+  password: "hr8jg8np58p64i5s",
+  database: "jxs72cljefh4uc4k"
 });
 
 
